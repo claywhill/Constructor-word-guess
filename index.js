09591e485157ac03195c5b2159814614934cd14a
Clayton Hill <clayman0123@yahoo.com>
2020-04-10 18:16:34 -0600

    inquirer prompt working

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-require("inquirer"); 
+var inquirer = require("inquirer"); 
+var word = require("./word");
+
+function start() {
+    inquirer.prompt([
+    {
+        type: "input",
+        message: "Guess a letter!",
+        name: "guess"
+    }  
+])
+.then(function(inquirerResponse) {
+    
+    if (inquirerResponse.guess === "a") {
+        console.log("True");
+    } else {
+        console.log("False");
+    }
+})
+}
+
+start();
+
--- word.js
+++ word.js
@@ -1,5 +1,5 @@
-require("./letter.js");
+var fs = require("fs");
+var letter = require("./letter");
 
-var Word = function what(x) {
-    
-}
+var words = ["The Beatles", "Pink Floyd", "The Who", "Rolling Stones", "Led Zeppelin", "Eagles", "Fleetwood Mac", "AC DC", "Cream", "Def Leppard", "Journey", "Queen", "Aerosmith", "ZZ Top", "Foreigner", "Creedence Clearwater Revial", "Lynyrd Skynyrd", "Black Sabbath", "Guns N Roses", "Blue Oyster Cult"];
+
